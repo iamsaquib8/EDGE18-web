23ac74eae89f15c52e3f3e15f85e193aaade4a2f
BeingSaquib <saquibulhassan6@gmail.com>
2018-01-04 02:59:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule
